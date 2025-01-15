07cf2f5fb2d2ccf15f7cca7d3897c4bf0905a3ad
Septiar <septiarmustafa@gmail.com>
2025-01-15 14:10:00 +0700

    drawer

---
+++ app/src/main/java/com/septiar/mynavigationdrawer/ui/subway/SubwayActivity.kt
@@ -0,0 +1,21 @@
+package com.septiar.mynavigationdrawer.ui.subway
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.septiar.mynavigationdrawer.R
+
+class SubwayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_subway)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
