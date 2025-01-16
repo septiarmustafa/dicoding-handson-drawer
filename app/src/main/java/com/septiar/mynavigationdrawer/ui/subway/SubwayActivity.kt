d1d803738b7c99569d8d9aba1d376cabc7def437
Septiar <septiarmustafa@gmail.com>
2025-01-16 08:06:23 +0700

    update subway activity

--- app/src/main/java/com/septiar/mynavigationdrawer/ui/subway/SubwayActivity.kt
+++ app/src/main/java/com/septiar/mynavigationdrawer/ui/subway/SubwayActivity.kt
@@ -12,10 +12,5 @@ class SubwayActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_subway)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
